302c2c9cff70eeb8ed095cd4de0cf7a832c8ef2e
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-15 01:22:04 +0000

    Automatically backed up by Learn

--- for.rb
+++ for.rb
@@ -3,7 +3,7 @@ def using_for
 	checklist = 1..10
 	for checklist in 1..10 do
 	#your code here
-	puts "Wingardium Leviosa"
+	puts "Wingardium Leviosa. The current number is #{checklist}."
 end
 end
 using_for
